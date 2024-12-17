d1b3a1d73310862ae3d582b8461b050a9e4503e1
wule <1744709282@qq.com>
2024-12-17 12:38:22 +0800

    doc FactoryBean

---
+++ springsourse-test/src/main/java/com/atguigu/spring/beans/Hello.java
@@ -0,0 +1,5 @@
+package com.atguigu.spring.beans;
+
+
+public class Hello {
+}
--- springsourse-test/src/main/java/com/atguigu/spring/beans/Person.java
+++ springsourse-test/src/main/java/com/atguigu/spring/beans/Person.java
@@ -12,6 +12,10 @@ import org.springframework.stereotype.Component;
  * Aware接口：帮我们装配Spring底层的一些组件
  * 1、Bean的功能增强全部都是有 BeanPostProcessor+InitializingBean（合起来完成的）
  * 2、骚操作就是 BeanPostProcessor+InitializingBean
+ *
+ *  你猜Autowired是怎么完成的
+ *  Person为什么能把ApplicationContext、MessageSource当为自己的参数传进来。
+ *   - 通过实现接口的方式自动注入了 ApplicationContext、MessageSource。是由BeanPostProcessor（Bean的后置处理器完成的）
  * @author wule
  * @create 2024-10-15 22:00
  */
